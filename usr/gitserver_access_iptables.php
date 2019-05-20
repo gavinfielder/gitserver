bae0e2542f0134baa1a6ec28f4ec5dc58ca4cbac
root <root@gitserver.42.us.org>
2019-05-20 15:26:22 -0700

    disallowed HTTPS port in firewall

--- usr/gitserver_access_iptables.php
+++ usr/gitserver_access_iptables.php
@@ -3,7 +3,7 @@
 function update_firewall(array $allowed_addresses)
 {
 	//The ports to open for the whitelisted addresses
-	$service_ports = [ 8080, 44343, 2222 ];
+	$service_ports = [ 8080, 2222 ];
 	//input/output files
 	$iptables_file = "/etc/iptables-rules";
 	$new_iptables = "/tmp/iptables-rules-new";
